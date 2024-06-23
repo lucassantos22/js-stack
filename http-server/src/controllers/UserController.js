aacc29481ed0621bd7616245bcdac9432d5885bd
lucassantos22 <lucas.afsantos@hotmail.com>
2024-06-23 19:57:46 -0300

    chore: import bodyParser helper

--- http-server/src/controllers/UserController.js
+++ http-server/src/controllers/UserController.js
@@ -21,19 +21,13 @@ module.exports = {
         res.send(200, user)
     },
     createUser: (req, res) => {
-        let body = ''
-        req.on('data', (chunk) => {
-            body += chunk
-        })
-        req.on('end', () => {
-            body = JSON.parse(body)
-            const lastUserId = users[users.length - 1].id
-            const newUser = {
-                id: lastUserId + 1,
-                name: body.name
-            }
-            users.push(newUser)
-            res.send(201, users)
-        })
+        const { body } = req;
+        const lastUserId = users[users.length - 1].id
+        const newUser = {
+            id: lastUserId + 1,
+            name: body.name
+        }
+        users.push(newUser)
+        res.send(201, users)
     }
 }
