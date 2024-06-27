bb0611400438d3501ac1c9c225e645813d91857e
lucassantos22 <lucas.afsantos@hotmail.com>
2024-06-26 21:42:13 -0300

    chore: add routes file

--- my-contacts/src/index.js
+++ my-contacts/src/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({
-        ok: true
-    })
-})
+const routes = require('./routes');
+
+app.use(routes)
 
 app.listen(8080, () => {
     console.log('Server started at http://localhost:8080')
