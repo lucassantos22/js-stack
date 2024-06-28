d374e6a983549786e9eeae0583509bf66c8a0cdb
lucassantos22 <lucas.afsantos@hotmail.com>
2024-06-28 18:55:53 -0300

    chore: add show route

--- my-contacts/src/routes.js
+++ my-contacts/src/routes.js
@@ -3,5 +3,6 @@ const router = Router();
 const ContactController = require('./app/controllers/ContactController')
 
 router.get('/contacts', ContactController.index)
+router.get('/contacts/:id', ContactController.show)
 
 module.exports = router;
