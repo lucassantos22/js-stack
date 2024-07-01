a2447e07b53e5c578c12ccdf3865b1d901b2381c
lucassantos22 <lucas.afsantos@hotmail.com>
2024-06-30 22:40:33 -0300

    chore: add put route

--- my-contacts/src/routes.js
+++ my-contacts/src/routes.js
@@ -5,6 +5,7 @@ const ContactController = require('./app/controllers/ContactController')
 router.get('/contacts', ContactController.index)
 router.get('/contacts/:id', ContactController.show)
 router.delete('/contacts/:id', ContactController.delete)
+router.put('/contacts/:id', ContactController.update)
 router.post('/contacts', ContactController.store)
 
 module.exports = router;
