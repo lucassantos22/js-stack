5f3e9e118adbd640c91e0c928c0bed40a81c2913
lucassantos22 <lucas.afsantos@hotmail.com>
2024-07-10 18:24:21 -0300

    chore: add left join to findAll

--- my-contacts/src/app/repositories/ContactsRepository.js
+++ my-contacts/src/app/repositories/ContactsRepository.js
@@ -4,7 +4,10 @@ class ContactsRepository {
     async findAll(orderBy = 'ASC') {
         const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
         const rows = await db.query(`
-            SELECT * FROM contacts ORDER BY name ${direction}
+            SELECT contacts.*, categories.name AS category_name
+            FROM contacts
+            LEFT JOIN categories ON categories.id = contacts.category_id
+            ORDER BY contacts.name ${direction}
         `)
         return rows
     }
