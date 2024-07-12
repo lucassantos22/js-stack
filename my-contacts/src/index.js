c389fca39d41000a87491878df8b2fd857629eac
lucassantos22 <lucas.afsantos@hotmail.com>
2024-07-11 22:48:16 -0300

    chore: add error handler

--- my-contacts/src/index.js
+++ my-contacts/src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+require('express-async-errors') // Precisa desse require para Error Handler funcionar em métodos assíncronos
 const app = express();
 
 const routes = require('./routes');
 
 app.use(express.json())
 app.use(routes)
+
+// Error handler -> Middleware de 4 argumentos que se comportará como error handler
+app.use((error, req, res, next) => {
+    console.log('### Error Handler ###')
+    console.log(error)
+    res.sendStatus(500)
+})
 
 app.listen(8080, () => {
     console.log('Server started at http://localhost:8080')
