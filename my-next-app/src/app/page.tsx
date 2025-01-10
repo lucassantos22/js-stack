a3963434e995e37b706258b008b3e72735d04e63
lucassantos22 <lucas.afsantos@hotmail.com>
2025-01-10 19:04:33 -0300

    chore

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -1,16 +1,20 @@
-async function getUsers() {
-  await new Promise(resolve => setTimeout(resolve, 2000));
-  return ['Lucas', 'Zezinho', 'Matheus']
-}
+// 'use client' -> Usar componente no modo CSR
 
 export default async function Home() {
+  async function getUsers() {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    return ['Lucas', 'Zezinho', 'Matheus']
+  }
+
+  console.log('Oi!')
+
   const users = await getUsers();
   return (
     <div>
       Hello World!
       <hr></hr>
-      {users.map(user => (
-        <p>{user}</p>
+      {users.map((user, i) => (
+        <p key={i}>{user}</p>
       ))}
     </div>
   );
