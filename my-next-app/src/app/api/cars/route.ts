e04876c1daa4fad25b6595b07d71581da7667b9b
lucassantos22 <lucas.afsantos@hotmail.com>
2025-01-15 19:36:52 -0300

    chore: add cookies

--- my-next-app/src/app/api/cars/route.ts
+++ my-next-app/src/app/api/cars/route.ts
@@ -1,7 +1,9 @@
+import { headers } from 'next/headers';
 import { type NextRequest, NextResponse } from 'next/server';
+
 export async function GET(req: NextRequest) {
-    const headers = new Headers(req.headers)
-    console.log(headers)
+    const reqHeaders = await headers();
+    console.log(reqHeaders.get('Content-Type'))
 
     return NextResponse.json({
         message: 'Hello world'
@@ -12,7 +14,12 @@ export async function POST(request: NextRequest) {
     const body = await request?.json();
     console.log(body)
 
-    return NextResponse.json({
-        create: true
-    }, { status: 201 })
+    console.log('id', request.nextUrl.searchParams.get('id'))
+
+    console.log('cookies', request.cookies.get(''))
+
+    const response = NextResponse.json({ create: true }, { status: 201 })
+    response.cookies.set('cookie-do-lucas', 'Cookie do Lucas')
+
+    return response;
 }
