bd48ae61bc0d7a12c63d0dfb94b7d40a596e62fd
lucassantos22 <lucas.afsantos@hotmail.com>
2025-01-27 00:42:12 -0300

    feat: add ContactsList style

---
+++ my-contacts/fe/src/components/ContactsList/styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    margin-top: 32px;
+`;
+
+export const Header = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    strong {
+        font-size: 24px;
+        color: #222;
+    }
+
+    a {
+        font-size: 16px;
+        color: ${props => props.theme.colors.primary.main};
+        text-decoration: none;
+        font-weight: bold;
+        border: 2px solid ${props => props.theme.colors.primary.main};
+        padding: 8px 16px;
+        border-radius: 4px;
+        transition: all 0.2s ease-in;
+
+        &:hover {
+            background-color: ${props => props.theme.colors.primary.main};
+            color: #fff;
+        }
+    }
+`
