dfb707848e7e363afcd6ab15d60496503209996a
lucassantos22 <lucas.afsantos@hotmail.com>
2025-01-27 00:42:41 -0300

    feat: add ContactsList

--- my-contacts/fe/src/assets/styles/global.js
+++ my-contacts/fe/src/assets/styles/global.js
@@ -10,7 +10,7 @@ export default createGlobalStyle`
 
     body {
         font-size: 16px;
-        background-color: ${props => props.theme.backgroundColor}
+        background-color: ${props => props.theme.colors.background}
     }
 
     button {
