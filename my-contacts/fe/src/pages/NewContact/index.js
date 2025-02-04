199e7be45c95cfdb3e1608090ec693b792279f28
lucassantos22 <lucas.afsantos@hotmail.com>
2025-02-04 18:47:13 -0300

    feat: add Select

--- my-contacts/fe/src/pages/NewContact/index.js
+++ my-contacts/fe/src/pages/NewContact/index.js
@@ -1,7 +1,19 @@
+import { Input } from "../../components/Input"
+import { Select } from "../../components/Select"
+
 import PageHeader from "../../components/PageHeader"
 
 export default function NewContact() {
     return (
-        <PageHeader title="Novo contato" />
+        <>
+            <PageHeader title="Novo contato" />
+            <Input
+                type="text"
+                placeholder="Nome"
+            />
+            <Select>
+                <option value="instagram">Instagram</option>
+            </Select>
+        </>
     )
 }
