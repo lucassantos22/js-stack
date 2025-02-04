afb1b6e2cab81cc1f13f6cc88e15de30534a1526
lucassantos22 <lucas.afsantos@hotmail.com>
2025-02-04 18:46:13 -0300

    feat: add Input

---
+++ my-contacts/fe/src/components/Input.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+    width: 100%;
+    background: #fff;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.04);
+    height: 52px;
+    border: 2px solid #fff;
+    border-radius:  4px;
+    outline: none;
+    padding: 0 16px;
+    font-size: 16px;
+    transition: all 0.1s ease-in;
+
+    &:focus {
+        border: 2px solid ${props => props.theme.colors.primary.main};
+    }
+`
