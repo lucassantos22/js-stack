1f855c229139a8588a2e347e38020fb40eb32944
lucassantos22 <lucas.afsantos@hotmail.com>
2025-02-09 16:23:24 -0300

    feat: add danger colors

--- my-contacts/fe/src/assets/styles/themes/default.js
+++ my-contacts/fe/src/assets/styles/themes/default.js
@@ -10,6 +10,11 @@ const defaultTheme =  {
         gray: {
             900: '#222',
             200: '#BCBCBC'
+        },
+        danger: {
+            light: '#F97171',
+            main: '#FC5050',
+            dark: '#F63131'
         }
     }
 }
